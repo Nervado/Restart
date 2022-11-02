e68da0790ee14e62d8cf08cac993678187325a35
Nervado <evandroabreudeoliveira@gmail.com>
2022-11-02 09:24:47 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Evandro  Abreu de Oliveira on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
